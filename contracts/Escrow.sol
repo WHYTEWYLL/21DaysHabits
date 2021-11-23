2e743cd52a8c267208d279927eadaeb6ce3bc372
Juan <juan@MacBook-Air-de-Juan.local>
2021-11-23 23:17:19 +0100

    test dev - until approve

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,26 +2,32 @@
 pragma solidity ^0.7.5;
 
 import "./interfaces/IERC20.sol";
+import "./interfaces/IWETHGateway.sol";
 import "./interfaces/ILendingPool.sol";
+import "hardhat/console.sol";
 
 contract Escrow {
+
     address arbiter;
     address inFavor;
     address opposing;
     uint public initialDeposit;
-    uint initTime;
+    uint public initTime;
     uint public timeChecker;
     uint rangeTime = 21 days;
 
-    enum Winner {P1, P2}
-
-    Winner[] public checks = new Winner[](21);
-
-    ILendingPool pool  = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
-    IERC20 aDai = IERC20(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
-    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    bool inited;
+    enum Winner {None, P1, P2}
     
-    constructor(address _inFavor, address _opposing, uint _amount)  {
+   IWETHGateway gateway;
+   IERC20 aWETH;
+    
+    Winner[] public checks = new Winner[](21);
+    
+    constructor( IWETHGateway _gateway, IERC20 _aWETH , address _inFavor, address _opposing, uint _amount )  {
+        aWETH = _aWETH;
+        gateway  = _gateway;
+
         arbiter = msg.sender;
         inFavor = _inFavor;
         opposing = _opposing;
@@ -30,20 +36,23 @@ contract Escrow {
    	}
     
     function inicialice() external{
+
       require(msg.sender == arbiter || msg.sender == inFavor || msg.sender == opposing ,"Not a member.");
       require(address(this).balance == initialDeposit, "Deposit not enough.");
 
       initTime = block.timestamp;
       timeChecker = block.timestamp;
-      //dai.transferFrom(msg.sender, address(this), initialDeposit);
-      //dai.approve(address(pool), initialDeposit);
-      //pool.deposit(address(dai), initialDeposit, address(this), 0);
+
+
+      gateway.depositETH{value: address(this).balance}( address(this), 0 );
+
+      inited = true;
     }
 
 
     function checker(Winner stateOfDay) external{
-      require(msg.sender == arbiter);
-      require(timeChecker + 1 days < block.timestamp);
+      require(msg.sender == arbiter, "Only the arbiter");
+      require(timeChecker + 1 days < block.timestamp, "One input per 24h");
 
       timeChecker = block.timestamp;
       checks.push(stateOfDay);
@@ -54,49 +63,49 @@ contract Escrow {
       uint8 fighter;
 
       for(uint i = 0; i < checks.length; i++){
+
         if(checks[i] == Winner.P1){
           fighter++;
-        }else{
+        } else if(checks[i] == Winner.P2){
           gambler++;
         }
       }
-      return (gambler, fighter);
+      return ( fighter , gambler );
     }
 
     event Approved();
 
-    function approve() external {
-        require(msg.sender == arbiter, "Approve must be called by the arbiter!");
-        require(initTime + rangeTime < block.timestamp);
+   function approve() external {
+    require(inited, "First you have to call inicialice");
+    console.log(initTime, rangeTime, block.timestamp);
+    require(msg.sender == arbiter, "Approve must be called by the arbiter!");
+    require(initTime + rangeTime < block.timestamp, "21 days minim");
+    console.log("1");
 
-        (uint8 opposingScore, uint8 inFavorScore) = score();
-
-        uint balance = aDai.balanceOf(address(this));
-
-        aDai.approve(address(pool), balance);
-
-        if (checks.length == opposingScore + inFavorScore){
-        
-          if (opposingScore > inFavorScore){
-            pool.withdraw(address(dai), initialDeposit, opposing);  
-          }
-          if ( inFavorScore > opposingScore){
-            pool.withdraw(address(dai), initialDeposit, inFavor);  
-          }
-
-          pool.withdraw(address(dai), type(uint).max, arbiter);
-        } else{
-          uint amountinvalid = type(uint).max / 2;
-          if (opposingScore > inFavorScore){
-            pool.withdraw(address(dai), amountinvalid, opposing);  
-          }
-          if ( inFavorScore > opposingScore){
-            pool.withdraw(address(dai), amountinvalid, inFavor);  
-          }
-        }
-
-    		emit Approved();
-    }
+   // (uint8 opposingScore, uint8 inFavorScore) = score();
+    // uint balance = aWETH.balanceOf(address(this));
+//
+   // aWETH.approve(address(gateway), balance);
+   // if(checks.length == opposingScore + inFavorScore){
+   // 
+   //   if (opposingScore > inFavorScore){
+   //     gateway.withdrawETH(initialDeposit, opposing);  
+   //   }
+   //   if ( inFavorScore > opposingScore){
+   //     gateway.withdrawETH( initialDeposit, inFavor);  
+   //   }
+   //   gateway.withdrawETH( type(uint).max, arbiter);
+   // } else{
+   //   uint amountinvalid = type(uint).max / 2;
+   //   if (opposingScore > inFavorScore){
+   //     gateway.withdrawETH(amountinvalid, opposing);  
+   //   }
+   //   if ( inFavorScore > opposingScore){
+   //     gateway.withdrawETH(amountinvalid, inFavor);  
+   //   }
+   // }
+   		emit Approved();
+   }
 
     receive() external payable { }
 
--- test/test.js
+++ test/test.js
@@ -2,14 +2,19 @@ const { BigNumber } = require("@ethersproject/bignumber");
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Escrow", function () {
+
+const IWETHGateway = "0xDcD33426BA191383f1c9B431A342498fdac73488";
+let aWETH  = "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e";
+
+describe("Escrow- Stage 1 - constructor and inicialice", function () {
     let escrow;
 
     beforeEach(async () => {
         [depositor, anFavor, opositor, tester] = await ethers.provider.listAccounts();
         const Escrow = await ethers.getContractFactory("Escrow");
-        escrow = await Escrow.deploy(anFavor, opositor, ethers.utils.parseEther("10"));
+        escrow = await Escrow.deploy( IWETHGateway, aWETH , anFavor, opositor, ethers.utils.parseEther("10"));
         await escrow.deployed();
+        aWETHcon = await ethers.getContractAt("IERC20", aWETH);
         
     });
 
@@ -18,16 +23,16 @@ describe("Escrow", function () {
         assert.equal(balance.toString(), "0");
     });
 
-    it("Inicial amunt should be 10 ", async function () {
+    it("initialDeposit amunt should be 10 ", async function () {
         const balance = await escrow.initialDeposit();
         assert.equal(balance.toString(), ethers.utils.parseEther("10"));
     });
 
-    it("Inicial amunt should be 10 ", async function () {
+    it("Balance should be 1 ", async function () {
         const [owner] = await ethers.getSigners();
         const transactionHash = await owner.sendTransaction({
             to: escrow.address,
-            value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+            value: ethers.utils.parseEther("1.0"), 
           });
         
         const balance = await ethers.provider.getBalance(escrow.address);
@@ -35,43 +40,141 @@ describe("Escrow", function () {
     });
 
     it("Initial amount no enought", async function () {
-        let ex;
-        try{
-            await escrow.inicialice();
-        }catch(_ex){
-            ex = _ex;
-        }
-        assert( ex, "Deposit not enough.");
+
+        await expect(escrow.inicialice()).to.be.revertedWith('Deposit not enough.');
     });
 
     it("initTime not a member", async function () {
-        let ex;
-        try{
-            await escrow.connect(tester).inicialice();
-        }catch(_ex){
-            ex = _ex;
-        }
-        console.log(ex);
-        assert( ex, "Not a member.");
+        const a = await ethers.getSigner(9);
+        
+        await expect( escrow.connect(a).inicialice()).to.be.revertedWith("Not a member.");
     });
 
-    it("timeChecker", async function () {
-        const [owner] = await ethers.getSigners();
+    it("timeChecker , all to aWETH", async function () {
+
+        const owner = await ethers.getSigner();
         const transactionHash = await owner.sendTransaction({
             to: escrow.address,
             value: ethers.utils.parseEther("10.0"), // Sends exactly 10.0 ether
           });
-        console.log(await ethers.provider.getBalance(escrow.address));
-        console.log(await escrow.initialDeposit());
+
+        const balance = await ethers.provider.getBalance(escrow.address);
 
         await escrow.inicialice();
-        
 
+        const balanceaWETH = await aWETHcon.balanceOf(escrow.address);
         
-        
-            
-        
+        expect(balanceaWETH.toString()).to.be.equal(balance.toString());
     });
 
 
 });
+
+describe("Escrow - Stage 2 - Checker & Score", function () {
+    
+    let escrow;
+
+    beforeEach(async () => {
+        [depositor, anFavor, opositor, tester] = await ethers.provider.listAccounts();
+        const Escrow = await ethers.getContractFactory("Escrow");
+        escrow = await Escrow.deploy( IWETHGateway, aWETH , anFavor, opositor, ethers.utils.parseEther("10"));
+        await escrow.deployed();
+        aWETHcon = await ethers.getContractAt("IERC20", aWETH);
+
+        const owner = await ethers.getSigner();
+        const transactionHash = await owner.sendTransaction({
+            to: escrow.address,
+            value: ethers.utils.parseEther("10.0"), // Sends exactly 10.0 ether
+          });
+        
+          await escrow.inicialice();
+    
+        });
+
+    it("Score should be 0,0", async function () {
+        const x = await escrow.score();
+        expect(x.toString()).to.be.equal("0,0");
+    });
+
+    it("Checker - Vote per 24 h ", async function () {
+        await expect( escrow.checker(1)).to.be.revertedWith("One input per 24h");
+        });
+
+    it("Checker - only arbiter ", async function () {
+        const tester = await ethers.getSigner(5);
+        await expect( escrow.connect(tester).checker(1)).to.be.revertedWith("Only the arbiter");
+        });
+
+    it("Checker - new vote in 24h", async function () {
+        const oneDay = 1 * 24 * 60 * 60;
+        await ethers.provider.send('evm_increaseTime', [oneDay]);
+        ethers.provider.send("evm_mine")    
+        await escrow.checker(1);
+        const score = await escrow.score();
+        expect( score.toString()).to.be.equal("1,0");
+
+    });
+    });
+
+    describe("Escrow - Stage 3 - Checker & Score", function () {
+        let escrow;
+
+        beforeEach(async () => {
+            [depositor, anFavor, opositor, tester] = await ethers.provider.listAccounts();
+            const Escrow = await ethers.getContractFactory("Escrow");
+            escrow = await Escrow.deploy( IWETHGateway, aWETH , anFavor, opositor, ethers.utils.parseEther("10"));
+            await escrow.deployed();
+            aWETHcon = await ethers.getContractAt("IERC20", aWETH);
+            const [owner] = await ethers.getSigners();
+            const transactionHash = await owner.sendTransaction({
+                to: escrow.address,
+                value: ethers.utils.parseEther("10.0"), 
+              });
+            
+
+        });
+
+        it("Checker - first inicialice ", async function () {
+
+            const tester = await ethers.getSigner(5);
+            const oneDay = 21 * 24 * 60 * 60;
+            await ethers.provider.send('evm_increaseTime', [oneDay]);
+            ethers.provider.send("evm_mine")  
+            await expect( escrow.approve()).to.be.revertedWith('First you have to call inicialice');
+            });
+
+        it("Checker - Approve must be called by the arbiter ", async function () {
+
+            await escrow.inicialice();
+
+            const tester = await ethers.getSigner(5);
+            const oneDay = 21 * 24 * 60 * 60;
+            await ethers.provider.send('evm_increaseTime', [oneDay]);
+            ethers.provider.send("evm_mine")  
+            await expect( escrow.connect( tester ).approve()).to.be.revertedWith('Approve must be called by the arbiter!');
+            });
+
+        it("Checker - 21 days minim ", async function () {
+
+            await escrow.inicialice();
+
+            const tester = await ethers.getSigner(5);
+            const oneDay = 2 * 24 * 60 * 60;
+            await ethers.provider.send('evm_increaseTime', [oneDay]);
+            ethers.provider.send("evm_mine")  
+            await expect( escrow.approve()).to.be.revertedWith('21 days minim');
+            });
+
+        it("Checker - only arbiter ", async function () {
+
+            await escrow.inicialice();
+
+            const tester = await ethers.getSigner(5);
+            const oneDay = 21 * 24 * 60 * 60;
+            await ethers.provider.send('evm_increaseTime', [oneDay]);
+            ethers.provider.send("evm_mine")  
+            await escrow.approve();
+            });
+
+    });
+    
